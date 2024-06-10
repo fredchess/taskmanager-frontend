1194b2e4a975394ef50269ed8d7be97721214c5c
Frederick Essono <fredericktabi30@gmail.com>
2024-06-10 15:20:55 +0100

    fix auth guard redirection

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,17 +1,18 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { LoginService } from './services/login/login.service';
 import { inject } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { catchError, map, of, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const loginService = inject(LoginService);
+  let auth = false;
 
   return loginService.getIsAuthenticated().pipe(map(user => {
-    if (!user) {
-      return router.createUrlTree(['/login']);
-    } else {
-      return true;
-    }
-  }))
+    return true
+  }), 
+    catchError(err => {
+      return of(router.createUrlTree(['/login']));
+    })
+  )
 };
